22d81ed0abf82398e0583acb0a6e74fe67d84ff3
BunniMK <86545296+BunniMK@users.noreply.github.com>
2021-11-12 14:57:42 -0700

    added semi-colons to cypress tests

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -13,7 +13,7 @@ describe('Form App', () => {
     const spinach = () => cy.get('input[name=spinach]');
     const zucchini = () => cy.get('input[name=zucchini]');
     const pineapple = () => cy.get('input[name=pineapple]');
-    const submitButton = () => cy.get('button[id="order-button"]')
+    const submitButton = () => cy.get('button[id="order-button"]');
     
     it('Initiation Check', () => {
         nameInput().should('exist');
@@ -31,40 +31,40 @@ describe('Form App', () => {
 
     describe('Can add text to text boxes', () => {
         it('Name text box works', () => {
-            nameInput().type('Person')
+            nameInput().type('Person');
             nameInput().should('have.value', 'Person');
         })
 
         it('Special request box works', () => {
-            specialInput().type('Please send paper plates')
+            specialInput().type('Please send paper plates');
             specialInput().should('have.value', 'Please send paper plates');
         })
     })
 
     describe('Multiple toppings can be selected', () => {
          it('Can select multiple toppings', () => {
-            mushrooms().click()
-            blackOlives().click()
-            onions().click()
-            greenPeppers().click()
-            spinach().click()
-            zucchini().click()
-            pineapple().click()
+            mushrooms().click();
+            blackOlives().click();
+            onions().click();
+            greenPeppers().click();
+            spinach().click();
+            zucchini().click();
+            pineapple().click();
          })    
     })
 
     describe('Can submit filled out form', () => {
         it('Fill out form and press submit', () => {
-            nameInput().type('Person')
-            specialInput().type('Please send paper plates')
-            mushrooms().click()
-            blackOlives().click()
-            onions().click()
-            greenPeppers().click()
-            spinach().click()
-            zucchini().click()
-            pineapple().click()
-            submitButton().click()
+            nameInput().type('Person');
+            specialInput().type('Please send paper plates');
+            mushrooms().click();
+            blackOlives().click();
+            onions().click();
+            greenPeppers().click();
+            spinach().click();
+            zucchini().click();
+            pineapple().click();
+            submitButton().click();
         })
     })
 
